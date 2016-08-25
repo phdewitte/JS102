e95a4a0f616341a8ab67f9f0377de8fb4ed22cd7
Patrick DeWitte <patrickhdewitte@gmail.com>
2016-08-25 16:06:13 -0500

    Complete objects fundamental practice

--- scripts.js
+++ scripts.js
@@ -1,3 +1,19 @@
-//Use this file to implement Part One of your project
+var animal = {
+  tagline: "I'm an animal called Jim",
+  noises: []
+}
 
+animal.username = "Jim"
 
+counter = 0
+
+for (var key in animal) {
+  counter++
+  console.log(counter);
+
+  if (key === 'username') {
+    console.log('Hi my name is ' + animal['username'])
+  } else if (key === 'tagline') {
+    console.log('I like to say ' + animal['tagline'])
+  };
+};
